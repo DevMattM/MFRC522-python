729d17d4d961f59c21660885d1c4cb19777ea228
Matthew McConnell <mjmcconnell@ufl.edu>
2019-06-24 23:57:18 -0400

    update names and ids

--- id_map.py
+++ id_map.py
@@ -1,27 +1,200 @@
 id_mapping = {
-    1 : {
+    967833896597 : {
         'text': 'This is a test to say stuff',
         'file_path': '/home/user/etc',
+        'name' : 'Dusty Rose',
         'count': 0
     },
-    2 : {
+    211667994202 : {
         'text': 'Say some more stuff and enjoy',
         'file_path': '/home/user/etc',
+        'name' : 'Matthew McConnell',
         'count': 0
     },
-    3 : {
+    280521688626 : {
         'text': 'this is for the third id and will count thrice',
         'file_path': '/home/user/etc',
+        'name' : 'Sarah Funaro',
         'count': 0
     },
-    4 : {
+    946038518753 : {
         'text': 'Fourth doesnt really count, but we will go with it anyway',
         'file_path': '/home/user/etc',
+        'name' : 'Sharmeelah Khemlani',
         'count': 0
     },
-    5 : {
+    1085020910374 : {
         'text': 'Fifth id and still going strong to test this out',
         'file_path': '/home/user/etc',
+        'name' : 'Stephanie McStravivk',
+        'count': 0
+    },
+    879483106173 : {
+        'text': 'Fifth id and still going strong to test this out',
+        'file_path': '/home/user/etc',
+        'name' : 'Samantha Lee',
+        'count': 0
+    },
+    122629534696 : {
+        'text': 'Fifth id and still going strong to test this out',
+        'file_path': '/home/user/etc',
+        'name' : 'Mina Ghorbani',
+        'count': 0
+    },
+    191315456990 : {
+        'text': 'Fifth id and still going strong to test this out',
+        'file_path': '/home/user/etc',
+        'name' : 'Valerie Moneayo',
+        'count': 0
+    },
+    97999611032 : {
+        'text': 'Fifth id and still going strong to test this out',
+        'file_path': '/home/user/etc',
+        'name' : 'Lynne Sommer',
+        'count': 0
+    },
+    1059988399205 : {
+        'text': 'Fifth id and still going strong to test this out',
+        'file_path': '/home/user/etc',
+        'name' : 'Marti Seawright',
+        'count': 0
+    },
+    1094650410410 : {
+        'text': 'Fifth id and still going strong to test this out',
+        'file_path': '/home/user/etc',
+        'name' : 'Matt Backensto',
+        'count': 0
+    },
+    53222060819 : {
+        'text': 'Fifth id and still going strong to test this out',
+        'file_path': '/home/user/etc',
+        'name' : 'Melissa Ghorbani',
+        'count': 0
+    },
+    967146030828 : {
+        'text': 'Fifth id and still going strong to test this out',
+        'file_path': '/home/user/etc',
+        'name' : 'Susan Rose',
+        'count': 0
+    },
+    605091869460 : {
+        'text': 'Fifth id and still going strong to test this out',
+        'file_path': '/home/user/etc',
+        'name' : 'Randy Dudenbostel',
+        'count': 0
+    },
+    830126507685 : {
+        'text': 'Fifth id and still going strong to test this out',
+        'file_path': '/home/user/etc',
+        'name' : 'Doug Rose',
+        'count': 0
+    },
+    235438564536 : {
+        'text': 'Fifth id and still going strong to test this out',
+        'file_path': '/home/user/etc',
+        'name' : 'Tracey Backensto',
+        'count': 0
+    },
+    830243948204 : {
+        'text': 'Fifth id and still going strong to test this out',
+        'file_path': '/home/user/etc',
+        'name' : 'Brad Delehanty',
+        'count': 0
+    },
+    647688315908 : {
+        'text': 'Fifth id and still going strong to test this out',
+        'file_path': '/home/user/etc',
+        'name' : 'Carol Quinn',
+        'count': 0
+    },
+    397859632005 : {
+        'text': 'Fifth id and still going strong to test this out',
+        'file_path': '/home/user/etc',
+        'name' : 'Judy Sailer',
+        'count': 0
+    },
+    486126470781 : {
+        'text': 'Fifth id and still going strong to test this out',
+        'file_path': '/home/user/etc',
+        'name' : 'Dee Delehanty',
+        'count': 0
+    },
+    692955989717 : {
+        'text': 'Fifth id and still going strong to test this out',
+        'file_path': '/home/user/etc',
+        'name' : 'Samuel Funaro',
+        'count': 0
+    },
+    967565461125 : {
+        'text': 'Fifth id and still going strong to test this out',
+        'file_path': '/home/user/etc',
+        'name' : 'Bill Murray',
+        'count': 0
+    },
+    279984817746: {
+        'text': 'Fifth id and still going strong to test this out',
+        'file_path': '/home/user/etc',
+        'name' : 'Jason Taylor',
+        'count': 0
+    },
+    5106910738: {
+        'text': 'Fifth id and still going strong to test this out',
+        'file_path': '/home/user/etc',
+        'name' : 'Ray Vann',
+        'count': 0
+    },
+    829975512796: {
+        'text': 'Fifth id and still going strong to test this out',
+        'file_path': '/home/user/etc',
+        'name' : 'Ronnie Dupuis',
+        'count': 0
+    },
+    605192532766: {
+        'text': 'Fifth id and still going strong to test this out',
+        'file_path': '/home/user/etc',
+        'name' : 'Jim McConnell',
+        'count': 0
+    },
+    898694989516: {
+        'text': 'Fifth id and still going strong to test this out',
+        'file_path': '/home/user/etc',
+        'name' : 'Rosemay Alcid',
+        'count': 0
+    },
+    880439472969: {
+        'text': 'Fifth id and still going strong to test this out',
+        'file_path': '/home/user/etc',
+        'name' : 'Aaron Delehanty',
+        'count': 0
+    },
+    348821734981 : {
+        'text': 'Fifth id and still going strong to test this out',
+        'file_path': '/home/user/etc',
+        'name' : 'Bobby McStravick',
+        'count': 0
+    },
+    810763629325 : {
+        'text': 'Fifth id and still going strong to test this out',
+        'file_path': '/home/user/etc',
+        'name' : 'Melissa Hamm',
+        'count': 0
+    },
+    967280248564 : {
+        'text': 'Fifth id and still going strong to test this out',
+        'file_path': '/home/user/etc',
+        'name' : 'Todd Delehanty',
+        'count': 0
+    },
+    674230645523 : {
+        'text': 'Fifth id and still going strong to test this out',
+        'file_path': '/home/user/etc',
+        'name' : 'Matt Hamm',
+        'count': 0
+    },
+    877319042033 : {
+        'text': 'Fifth id and still going strong to test this out',
+        'file_path': '/home/user/etc',
+        'name' : 'Gladys Moncayo',
         'count': 0
     },
 }
